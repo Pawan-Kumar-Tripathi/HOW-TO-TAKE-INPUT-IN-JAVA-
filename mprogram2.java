309765ba89296ecb8ce83e6ce81c671c319bdf31
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 21:38:08 +0530

    Add files via upload

---
+++ mprogram2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class mprogram2
+{
+    public void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number 1 ");
+        int a= sc.nextInt();
+        System.out.println("enter the number 2 ");
+        int b= sc.nextInt();
+        int mult = a*b;
+        System.out.println("The multiplication of the number is ="+ mult);
+    }
+}
+        
